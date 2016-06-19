345290c06a05e8fc0672df2b428cbcd73086c90a
jacquestkirk <jacquestkirk@gmail.com>
2016-06-19 00:39:27 -0500

    Add additional thermistor options. Plug them into thermistor error calculator

--- Matlab/ThermistorError.m
+++ Matlab/ThermistorError.m
@@ -1,21 +1,23 @@
-load 'TS91ResTemp'
-temps_degC = TS91ResTemp(:,1);
-resistances_ohms = TS91ResTemp(:,2);
+%load 'TS91ResTemp'
+%load 'B57164K0102'
+load 'NTCLE203E3302SB0'
+temps_degC = data(:,1);
+resistances_ohms = data(:,2);
 
 
 
 %%
-ref_res_ohms = 68000;
+ref_res_ohms = 51000;
 
 
 pct_full_scale = ref_res_ohms./(resistances_ohms + ref_res_ohms) * 100;
 
 
-temp_res = temps_degC(1)-temps_degC(2);
+temp_res = temps_degC(2)-temps_degC(1);
 temp_centered_degC = temps_degC + temp_res;
 temp_centered_degC = temp_centered_degC(1:end-1);
 
-pct_full_scale_per_degC = -diff(pct_full_scale)/temp_res;
+pct_full_scale_per_degC = diff(pct_full_scale)/temp_res;
 
 figure(1)
 plot(temps_degC, pct_full_scale)
@@ -23,8 +25,13 @@ ylabel('Percentage of full scale (%)')
 xlabel('Temp (degC)')
 figure(2)
 plot(temp_centered_degC, pct_full_scale_per_degC)
-xlim([-40,40])
+xlim([-55,40])
 ylabel('Percentage change per deg C (%)')
+xlabel('Temp (degC)')
+
+figure(3)
+plot(temps_degC, resistances_ohms)
+ylabel('Resistance (Ohms)')
 xlabel('Temp (degC)')
 
 
