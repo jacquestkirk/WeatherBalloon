8fd453911cd3377376237f6b2b21acdb89ee0e4d
jacquestkirk <jacquestkirk@gmail.com>
2017-03-11 18:35:46 -0600

    Add Led functionality

--- Firmware/WeatherBalloon/inc/Sensors/led.h
+++ Firmware/WeatherBalloon/inc/Sensors/led.h
@@ -11,9 +11,18 @@
 void Led_Blink_1(void);
 void Led_Blink_2(void);
 
+void Led_Toggle_1(void);
+void Led_Toggle_2(void);
+
 void Led_1_On_Tsk(void);
 void Led_2_On_Tsk(void);
 void Led_1_Off_Tsk(void);
 void Led_2_Off_Tsk(void);
+
+void Led_On_1(void);
+void Led_Off_1(void);
+void Led_On_2(void);
+void Led_Off_2(void);
+
 
 #endif /* LED_H_ */
--- Firmware/WeatherBalloon/src/Sensors/led.c
+++ Firmware/WeatherBalloon/src/Sensors/led.c
@@ -7,6 +7,10 @@
 
 
 #include "Sensors/led.h"
+#include "em_gpio.h"
+
+uint8_t _led1_state = 0;
+uint8_t _led2_state = 1;
 
 void Led_Blink_1(void)
 {
@@ -35,4 +39,51 @@ void Led_1_Off_Tsk(void)
 void Led_2_Off_Tsk(void)
 {
 	//Todo: It would be nice if we actually did something here.
+}
+
+void Led_On_1(void)
+{
+	GPIO_PinOutClear(gpioPortC, 2);
+	_led1_state = 1;
+}
+
+void Led_Off_1(void)
+{
+	GPIO_PinOutSet(gpioPortC,2);
+	_led1_state = 0;
+}
+void Led_On_2(void)
+{
+	GPIO_PinOutClear(gpioPortC, 3);
+	_led2_state = 1;
+}
+
+void Led_Off_2(void)
+{
+	GPIO_PinOutSet(gpioPortC,3);
+	_led2_state = 1;
+}
+
+void Led_Toggle_1(void)
+{
+	if(_led1_state)
+	{
+		Led_Off_1();
+	}
+	else
+	{
+		Led_On_1();
+	}
+}
+
+void Led_Toggle_2(void)
+{
+	if(_led2_state)
+	{
+		Led_Off_2();
+	}
+	else
+	{
+		Led_On_2();
+	}
 }
