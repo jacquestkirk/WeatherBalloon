b71900b731710e413683ba81dbe016d530dd0d1a
jacquestkirk <jacquestkirk@gmail.com>
2017-03-11 20:15:35 -0600

    Initial commit of backup timer

---
+++ Firmware/WeatherBalloon/inc/BackupTimer.h
@@ -0,0 +1,13 @@
+/*
+ * BackupTimer.h
+ *
+ *  Created on: Mar 11, 2017
+ *      Author: Summer
+ */
+
+#ifndef BACKUPTIMER_H_
+#define BACKUPTIMER_H_
+
+void BackupTimer_Init(void);
+
+#endif /* BACKUPTIMER_H_ */
