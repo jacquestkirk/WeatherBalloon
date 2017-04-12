85949aefa36217e2830ce74bb4c9db1f516da010
mubashir84 <smanwar84@hotmail.com>
2017-04-11 22:04:16 -0500

    Integrated SPIBubbl driver moduel into Flash module

--- Firmware/WeatherBalloon/inc/flash.h
+++ Firmware/WeatherBalloon/inc/flash.h
@@ -10,21 +10,37 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include "spidrv.h"
+#include "em_gpio.h"
+
 
 #define FLASH_PAGE_SIZE_BYTES 256
 
 // Flash data page0 per dataset
-#define FLASH_PAGE0_TIMESTAMP 0
-#define FLASH_PAGE0_IMU 490
-#define FLASH_PAGE0_MAGNETOMETER 31412
-#define FLASH_PAGE0_PRESSURE 31441
-#define FLASH_PAGE0_TEMP_PRESSURE 31931
-#define FLASH_PAGE0_TEMP1 32421
-#define FLASH_PAGE0_TEMP2 32445
-#define FLASH_PAGE0_TEMP3 32469
-#define FLASH_PAGE0_TEMP4 32493
-#define FLASH_PAGE0_TEMP_MCU 32517
-#define FLASH_PAGE0_ERR_CODES 32766
+#define FLASH_PAGE0_BOOT_COUNT 0
+#define FLASH_PAGELAST_BOOT_COUNT 1
+#define FLASH_PAGE0_ERR_CODES 2
+#define FLASH_PAGELAST_ERROR 255
+#define FLASH_PAGE0_TIMESTAMP 256
+#define FLASH_PAGELAST_TIMESTAMP 741
+#define FLASH_PAGE0_IMU 742
+#define FLASH_PAGELAST_IMU 31422
+#define FLASH_PAGE0_MAGNETOMETER 31423
+#define FLASH_PAGELAST_MAGNETOMETER 31450
+#define FLASH_PAGE0_PRESSURE 31451
+#define FLASH_PAGELAST_PRESSURE 31937
+#define FLASH_PAGE0_TEMP_PRESSURE 31938
+#define FLASH_PAGELAST_TEMP_PRESSURE 32423
+#define FLASH_PAGE0_TEMP1 32424
+#define FLASH_PAGELAST_TEMP1 32447
+#define FLASH_PAGE0_TEMP2 32448
+#define FLASH_PAGELAST_TEMP2 32470
+#define FLASH_PAGE0_TEMP3 32471
+#define FLASH_PAGELAST_TEMP3 32494
+#define FLASH_PAGE0_TEMP4 32495
+#define FLASH_PAGELAST_TEMP4 32517
+#define FLASH_PAGE0_TEMP_MCU 32518
+#define FLASH_PAGELAST_TEMP_MCU 32767
 
 typedef enum {
     ALLZEROS,
@@ -32,6 +48,7 @@ typedef enum {
     RANDOM
 } Flash_Enum_Test_Data_type;
 
+void Flash_SPI_Initalize();
 void Flash_Initialize();
 void Flash_Run_Periodic_Task();
 void Flash_Populate_Write_Buffer();
@@ -40,13 +57,13 @@ void Flash_Write_Imu();
 void Flash_Write_Mag();
 void Flash_Write_Pressure();
 void Flash_Write_Temp();
-void Flash_Write_Page0Test();
+void Flash_Populate_Write_Buffer();
 void Flash_Write_Page(int pagenum);
 void Flash_Write_Page_Test_Data(int pagenum, Flash_Enum_Test_Data_type write_dataset);
-uint8_t Flash_Read_FirstByte(int pagenum);
+uint8_t * Flash_Read_FirstByte(int pagenum);
 uint8_t * Flash_Read_Page(int pagenum);
 uint8_t * Flash_Read_ID();
-uint8_t * Flash_GetWriteBufferAddress();
+uint8_t * Flash_Read_Status();
 uint16_t Flash_Get_ResetCount();
 
 
--- Firmware/WeatherBalloon/src/flash.c
+++ Firmware/WeatherBalloon/src/flash.c
@@ -8,7 +8,7 @@
 
 #include "flash.h"
 #include "timestamp.h"
-#include "SPIBubbl.h"
+//#include "SPIBubbl.h"
 #include "Sensors/imu.h"
 #include "Sensors/magnetometer.h"
 #include "Sensors/pressure.h"
@@ -47,10 +47,42 @@ int Flash_Offset_Temp_MCU = 0;
 
 uint8_t page_read[256];
 uint8_t SPIWrite[260];
-uint8_t FlashRead[260];
+uint8_t FlashReadPage[260];
+uint8_t FlashReadByte[5];
 uint8_t FlashReadID[4];
+uint8_t FlashReadStatus[2];
 uint8_t *Flash_data_to_write;
 // Add a few dummy arrays that can be written to flash via CLI command
+
+
+SPIDRV_HandleData_t handleData;
+SPIDRV_Handle_t handle = &handleData;
+
+// Initilize SPI
+void Flash_SPI_Initalize()
+{
+	// Set up the necessary peripheral clocks
+	CMU_ClockEnable(cmuClock_GPIO, true);
+
+	GPIO_DriveModeSet(gpioPortA, gpioDriveModeLow);
+
+	// Enable the GPIO pins for the misc signals, leave pulled high
+	GPIO_PinModeSet(gpioPortA, 9, gpioModePushPullDrive, 1);          // WP#
+	GPIO_PinModeSet(gpioPortA, 10, gpioModePushPullDrive, 1);          // HOLD#
+
+	//GPIO_PinModeSet(gpioPortE, 13, gpioModePushPullDrive, 1); // /cs = 1
+	//GPIO_PinModeSet(gpioPortE, 13, gpioModePushPullDrive, 0); // /cs = 0
+	//GPIO_PinModeSet(gpioPortE, 10, gpioModePushPullDrive, 1); // MOSI = 1
+	//GPIO_PinModeSet(gpioPortE, 10, gpioModePushPullDrive, 0); // MOSI = 0
+
+    // Initialize and enable the SPIDRV
+	SPIDRV_Init_t initData = SPIDRV_MASTER_USART0;
+	initData.clockMode = spidrvClockMode0;  // Flash can operate in mode 0 or 1
+	initData.portLocation = _USART_ROUTE_LOCATION_LOC0;
+
+	// Initialize a SPI driver instance
+	SPIDRV_Init( handle, &initData );
+}
 
 
 /* void Flash_Initialize()
@@ -61,6 +93,8 @@ uint8_t *Flash_data_to_write;
  */
 void Flash_Initialize()
 {
+
+	Flash_SPI_Initalize();
 
 	Flash_Current_Write_Block = Flash_Currently_Writing_Nothing;
 
@@ -139,7 +173,6 @@ void Flash_Run_Periodic_Task()
 		*/
 	}
 
-
 	if (Imu_QueryReadyToWriteFlashFlag())
 	{
 		if (FLASH_PAGE0_IMU + Flash_Offset_Imu < FLASH_PAGE0_MAGNETOMETER)
@@ -179,13 +212,14 @@ void Flash_Run_Periodic_Task()
 void Flash_Write_Time()
 {
 	// Ping Query... If ready... then
-	//if (Time_QueryReadyToWriteFlashFlag())
+	 if (TimeStamp_QueryReadyToWriteFlashFlag())
 	{
 		// getbufferaddress... ask IMU block which of the two buffers to get data from
-		// Write the data by invoking FlashWritePage()
-		// ... Then when all good, clearreadytowrite in IMU module
-
-		//Time_GetBufferAddress(void)
+		Flash_data_to_write = TimeStamp_GetBufferAddress();
+		Flash_Populate_Write_Buffer();
+		// Write the data by invoking
+		Flash_Write_Page(FLASH_PAGE0_TIMESTAMP + Flash_Offset_Timestamp);
+		//Time_ClearReadyToWriteFlashFlag();
 
 	}
 
@@ -228,7 +262,9 @@ void Flash_Write_Temp()
 
 void Flash_Populate_Write_Buffer()
 {
+	// Byte4 = DATA0 = 0x00 (DATA0 byte is Page written status byte).
 	SPIWrite[4] = 0x00;
+
 	for(int k = 5; k < (FLASH_PAGE_SIZE_BYTES+4); k++)
 	{
 		SPIWrite[k] = Flash_data_to_write[k-5];
@@ -238,6 +274,10 @@ void Flash_Populate_Write_Buffer()
 // Sets up and invoke SPI command to write a full page in flash
 void Flash_Write_Page(int pagenum)
 {
+	int size = 260;
+	uint8_t dummy_rx[size];
+	uint8_t write_en[2];
+
 	// Flash command: Byte0 = Write Instruction = 0x02.
 	SPIWrite[0] = 0x02;
 
@@ -246,11 +286,21 @@ void Flash_Write_Page(int pagenum)
 	SPIWrite[2] = (pagenum >> 8) && 0xFF;
 	SPIWrite[1] = (pagenum >> 16) && 0xFF;
 
-	// Byte4 = DATA0 = 0x00 (DATA0 byte is Page written status byte).
-	SPIWrite[4] = 0x00;
 
-	// Invoke SPI write with *SPIWrite
-	SPIBubbl_Transmit(SPIWrite, 260);
+	// Write Enable
+	write_en[0] = 0x06; // Write Enable Command
+	SPIDRV_MTransferB(handle, &write_en, FlashReadStatus, size);
+
+	// Write page
+	SPIDRV_MTransferB(handle, &SPIWrite, &dummy_rx, size);
+
+	// Wait in while() loop until Flash status is no longer writing
+	do
+	{
+	}while (Flash_Read_Status() && 0x01);
+
+	// At this point clearreadytowrite()
+
 }
 
 // Writes test data of a type defined in Flash_Enum_Test_Data_type in page # pagenum
@@ -280,39 +330,56 @@ void Flash_Write_Page_Test_Data(int pagenum, Flash_Enum_Test_Data_type write_dat
 
 
 // Returns byte .. Used in normal mode of operation
-uint8_t Flash_Read_FirstByte(int pagenum)
+uint8_t * Flash_Read_FirstByte(int pagenum)
 {
-	uint8_t readbyte;
-	// Read byte 0 at pagenum
-	readbyte = 0x00;
-	return readbyte;
+	int size = 5;
+	uint8_t tx_data[size];
+	tx_data[0] = 0x03; // Flash Read Command
+	// txdata[1,2,3] = pagenum
+
+	SPIDRV_MTransferB( handle, &tx_data, FlashReadByte, size);
+
+	return &FlashReadByte[4];
+
 }
 
 // Only used for CLI mode of operation
 uint8_t * Flash_Read_Page(int pagenum)
 {
-	// Do the reading at pagenum and store into FlashRead[]
-	// Then copy FlashRead[4..259] to page_read[0..255]
+	// Do the reading at pagenum and store into FlashReadPage[]
+	// Then return &FlashReadPage[4]
+	int size = 260;
+	uint8_t tx_data[size];
+	tx_data[0] = 0x03; // Flash Read Command
+	// txdata[1,2,3] = pagenum
 
-	for(int i=0; i<=255; i++)
-	{
-		page_read[i] = FlashRead[i+4];
-	}
-	return page_read;
+	SPIDRV_MTransferB( handle, &tx_data, FlashReadPage, size);
+
+	return &FlashReadPage[4];
 }
 
 // Returns first 3 bytes of Flash Identification
 // 0x20 20 17
 uint8_t * Flash_Read_ID()
 {
-	// Do the reading at pagenum and store into FlashRead[]
-	// Then copy FlashRead[4..6] to page_read[0..2]
-
-	SPIBubbl_Initalize();
-
-	SPIBubbl_Read_ID(FlashReadID);
+//	SPIBubbl_Initalize();
+	int size = 4;
+	uint8_t tx_data[size];
+	tx_data[0] = 0x9F; //JEDEC_ID_CMD -- DUNNO if command it's JEDEC constant or not
+	SPIDRV_MTransferB( handle, &tx_data, FlashReadID, size);
 
 	return &FlashReadID[1];
+}
+
+uint8_t * Flash_Read_Status()
+{
+      int size = 2;
+      uint8_t tx_data[size];
+
+      tx_data[0] = 0x05; // Flash Status Command
+
+      SPIDRV_MTransferB( handle, &tx_data, FlashReadStatus, size);
+      return &FlashReadStatus[1];
 }
 
 uint16_t Flash_Get_ResetCount()
